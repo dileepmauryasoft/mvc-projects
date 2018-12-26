46ac70d041ed5a48604de986427da0fae3421a8c
Dileep Kumar Maurya <dileepmauryasoft@gmail.com>
2018-12-26 15:59:06 +0530

    test

--- eCommerce/eCommerce.Web/Controllers/HomeController.cs
+++ eCommerce/eCommerce.Web/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace eCommerce.Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page. testing";
+            ViewBag.Message = "Your application description page. testing testing ";
 
             return View();
         }
